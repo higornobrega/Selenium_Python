6bd4532f20034ab148a2ac7a28e7b5c663ea5668
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-10-01 10:45:00 -0300

    Introdução

--- aula3/aula3.py
+++ aula3/aula3.py
@@ -1,10 +1,23 @@
-from time import time
 from selenium.webdriver import Chrome
 #from selenium.webdriver import Firefox
+from time import sleep
 
-import time 
+url = 'https://curso-python-selenium.netlify.app/aula_03.html'
 
 navegador = Chrome()
 #navegador = Firefox()
-time.sleep(2)
+navegador.get(url)
+sleep(3)
+
+a = navegador.find_element_by_tag_name('a')
+
+for click in range(10):
+    ps = navegador.find_elements_by_tag_name('p')
+    a.click()
+    print(f'{ps[-1].text}, {click}')
+    print(f'{ps[-1].text == click}')
+    print(f'{ps[-1].text == str(click)}')
+
+print(f'{a.text}')
+
 navegador.quit()
